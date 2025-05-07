7ac6ac2152eeb0038a3efc5ab0145e5d34205b06
hilalhamdh <hilalhamdihafit@gmail.com>
2025-05-07 21:42:11 +0800

    add file

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="grid grid-col1-1 md:grid-cols-3 gap-5">
           <div className="mx-5 text-white md:ml-12 pt-14">
             <h2 className="font-bold text-2xl md:text-3xl">About Company</h2>
-            <p className="pt-5 md:pt-10 text-lg md:text-xl text-justify  md:mr-12">
+            <p className="pt-5 md:pt-10 text-sm md:text-lg text-justify  md:mr-12">
               Charcoal Premium has been a leading global charcoal manufacturer
               and exporter for the past 6 years, delivering top-quality
               varieties of Indonesian natural wooden and wooden briquette
@@ -18,14 +18,14 @@ const Footer = () => {
             <h2 className="font-bold text-2xl md:text-3xl mx-5 md:mx-16">
               Contact
             </h2>
-            <p className="pt-5 md:pt-10 text-lg md:text-xl text-justify mx-5  md:mx-16 ">
+            <p className="pt-5 md:pt-10 text-sm md:text-lg text-justify mx-5  md:mx-16 ">
               Address : Ponrang, Desa Tirowali, Kecamatan Ponrang, Kabupaten
               Luwu, Sulawesi Selatan
             </p>
             <h2 className="font-bold text-xl pt-2 md:pt-4 mx-5 md:mx-16">
               Email
             </h2>
-            <p className="pt-1 md:pt-3 text-lg text-justify mx-5 md:mx-16 ">
+            <p className="pt-1 md:pt-3 text-sm text-justify mx-5 md:mx-16 ">
               barapremium@gmail.com
             </p>
             <h2 className="font-bold text-xl pt-1 md:pt-4 mx-5 md:mx-16">
