7bd5cc05aab396e5f210c635c83b1dffcd5d7837
hilalhamdh <hilalhamdihafit@gmail.com>
2025-06-13 11:47:40 +0800

    upload file.txt

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,25 @@ function Contact() {
       <NavbarPages />
       <div className="homepage pb-10">
         <div className="container mx-auto px-4 ">
-          <div className="Contact grid md:grid-cols-2  grid-cols-1 items-center gap-3 pt-20 md:pt-32">
+          <div className=" bg-gradient-to-r from-slate-200 to-gray-400 to-90% h-60 md:h-70 pt-3  ">
+            <p className="text-2xl  md:text-6xl font-bold mx-4 md:mx-10 pt-16 md:pt-24 pb-2  ">
+              FIRECITY TRAD
+            </p>
+            <p className=" mt-1 text-sm md:text-xl  text-justify px-4 sm:pl-8 mb-4">
+              We are a factory wood charcoal. We produce the best wood charcoal
+              with the best quality raw material
+            </p>
+            <ul className="flex mx-4 md:mx-10 w-60 md:w-70 h-9 rounded-sm  bg-slate-300">
+              <li className="text-yellow-900 px-5 pt-1 text-center items-center font-semibold text-lg md:text-xl">
+                <a href="/">Home </a>
+              </li>
+              <p className="py-2 pr-3 text-sm font-bold">||</p>
+              <li className="text-yellow-900 pt-1 text-center items-center font-semibold text-lg md:text-xl">
+                <a href="/product">Our Products</a>
+              </li>
+            </ul>
+          </div>
+          <div className="Contact grid md:grid-cols-2  grid-cols-1 items-center gap-3 pt-10 ">
             <div className="grid-cols-3 w-[350px] md:w-2xl h-72   shadow-black border-black border-4 rounded-4xl items-start mx-auto">
               <p className="mt-7 border-2 bg-sky-200 text-black border-black text-xl text-center items-center mx-auto rounded-xl md:rounded-lg  w-40  h-8 font-semibold">
                 Call Us
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,7 +9,7 @@ function Product() {
       <NavbarPages />
       <div className="product pb-10 ">
         <div className="container mx-auto  ">
-          <div className=" items-center pt-14  md:pt-32">
+          <div className=" items-center ">
             <div className=" bg-gradient-to-r from-slate-200 to-gray-400 to-90% h-60 md:h-70 pt-3  ">
               <p className="text-2xl  md:text-6xl font-bold mx-4 md:mx-10 pt-16 md:pt-24 pb-2  ">
                 FIRECITY TRAD
